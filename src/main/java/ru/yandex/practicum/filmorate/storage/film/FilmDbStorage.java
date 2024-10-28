6f5c052b30c0182acae905c88fe3d3499466e872
klv2503 <168983480+klv2503@users.noreply.github.com>
2024-10-29 01:04:55 +0200

    Update FilmDbStorage.java
    потерял плюс

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -81,7 +81,7 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
             "(SELECT fd.film_id FROM films_directors AS fd WHERE fd.director_id IN " +
             "(SELECT d.id FROM directors AS d WHERE LOWER(d.name) LIKE ?)) " +
             "ORDER BY f.release_date DESC";
-    private static final String FIND_ALL_BY_TITLE_AND_DIRECTOR_CONTEXT = "SELECT DISTINCT f.* FROM FILMS f " 
+    private static final String FIND_ALL_BY_TITLE_AND_DIRECTOR_CONTEXT = "SELECT DISTINCT f.* FROM FILMS f " +
             "LEFT JOIN FILMS_DIRECTORS fd ON f.ID = fd.FILM_ID " +  
             "LEFT JOIN DIRECTORS d ON fd.DIRECTOR_ID = d.ID " +
             "WHERE f.TITLE LIKE ? OR d.NAME LIKE ?";
