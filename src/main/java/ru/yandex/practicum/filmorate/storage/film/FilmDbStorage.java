206fb47c1be5a36d230f97c3261e6769f25cb4cc
klv2503 <168983480+klv2503@users.noreply.github.com>
2024-10-29 01:17:21 +0200

    Update FilmDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -75,16 +75,21 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
             "LIMIT ?";
 
     private static final String FIND_ALL_BY_TITLE_CONTEXT = "SELECT * FROM " + tableName +
-            " WHERE LOWER(films.title) LIKE ? ORDER BY release_date DESC";
+            " WHERE LOWER(films.title) LIKE ? ORDER BY title DESC";
     private static final String FIND_ALL_BY_DIRECTOR_CONTEXT = "SELECT * FROM films AS f" +
             " WHERE f.id IN " +
             "(SELECT fd.film_id FROM films_directors AS fd WHERE fd.director_id IN " +
             "(SELECT d.id FROM directors AS d WHERE LOWER(d.name) LIKE ?)) " +
-            "ORDER BY f.release_date DESC";
-    private static final String FIND_ALL_BY_TITLE_AND_DIRECTOR_CONTEXT = "SELECT DISTINCT f.* FROM FILMS f " +
-            "LEFT JOIN FILMS_DIRECTORS fd ON f.ID = fd.FILM_ID " +
-            "LEFT JOIN DIRECTORS d ON fd.DIRECTOR_ID = d.ID " +
-            "WHERE f.TITLE LIKE ? OR d.NAME LIKE ?";
+            "ORDER BY f.title DESC";
+    private static final String FIND_ALL_BY_TITLE_AND_DIRECTOR_CONTEXT =
+            "SELECT f.*, COUNT(fl.user_id) AS likes_count " +
+                    "FROM film_likes fl " +
+                    "RIGHT JOIN films f ON fl.film_id = f.id " +
+                    "WHERE (LOWER(f.title) LIKE ?) OR " +
+                    "f.id IN (SELECT fd.film_id FROM films_directors AS fd " +
+                    "WHERE fd.director_id IN (SELECT d.id FROM directors AS d WHERE LOWER(d.name) LIKE ?)) " +
+                    "GROUP BY f.id " +
+                    "ORDER BY likes_count DESC";
 
     private static final String FIND_RECOMMENDED_FOR_USER_QUERY = "SELECT " + fields + " FROM " + tableName + " " +
             """
