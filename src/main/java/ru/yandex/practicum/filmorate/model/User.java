f109b2968cfc69faa4b5542a0674d8a47f4e655f
lazyabrikos <vovager2003@yandex.ru>
2024-10-31 20:07:10 +0300

    NoArgConstructor

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,10 +2,12 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 public class User {
 
     private Integer id;
@@ -23,8 +25,6 @@ public class User {
     @PastOrPresent
     private LocalDate birthday;
 
-    public User() {
-    }
 
     public User(String login, String name, String email, LocalDate birthday) {
         this.login = login;
