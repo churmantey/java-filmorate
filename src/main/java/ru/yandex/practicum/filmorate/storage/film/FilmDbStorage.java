4c88fe03ef74b27d7fb5ad2d3d15c3eba7ed52b4
Лев Курас <levkuras@yandex.kz>
2024-11-03 18:02:15 +0100

    В поиск по контексту добавил сортировку по id после avg_rate

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -92,7 +92,7 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
             "RIGHT JOIN films f ON fl.film_id = f.id " +
             "WHERE LOWER(f.title) LIKE ? " +
             "GROUP BY f.id " +
-            "ORDER BY avg_rate DESC";
+            "ORDER BY avg_rate DESC, f.id";
     private static final String FIND_ALL_BY_DIRECTOR_CONTEXT = "SELECT f.*, AVG(fl.rate) AS avg_rate " +
             "FROM film_likes fl " +
             "RIGHT JOIN films f ON fl.film_id = f.id " +
@@ -100,7 +100,7 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
             "(SELECT fd.film_id FROM films_directors AS fd " +
             "WHERE fd.director_id IN (SELECT d.id FROM directors AS d WHERE LOWER(d.name) LIKE ?)) " +
             "GROUP BY f.id " +
-            "ORDER BY avg_rate DESC";
+            "ORDER BY avg_rate DESC, f.id";
     private static final String FIND_ALL_BY_TITLE_AND_DIRECTOR_CONTEXT =
             "SELECT f.*, AVG(fl.rate) AS avg_rate " +
                     "FROM film_likes fl " +
@@ -109,7 +109,7 @@ public class FilmDbStorage extends BaseDbStorage<Film> implements FilmStorage {
                     "f.id IN (SELECT fd.film_id FROM films_directors AS fd " +
                     "WHERE fd.director_id IN (SELECT d.id FROM directors AS d WHERE LOWER(d.name) LIKE ?)) " +
                     "GROUP BY f.id " +
-                    "ORDER BY avg_rate DESC";
+                    "ORDER BY avg_rate DESC, f.id";
 
     private static final String FIND_RECOMMENDED_FOR_USER_QUERY = "SELECT " + fields + " FROM " + tableName + " " +
             """
